f219a79907a4379aa65502a2ca0b7b9899c1f4a0
aa-nadim <nadim.ice.nstu@gmail.com>
2021-03-26 19:05:58 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCeBjQYGfbbGD9p2S9XsiDcL-_R_eJrXYY",
+    authDomain: "burj-al-ar.firebaseapp.com",
+    projectId: "burj-al-ar",
+    storageBucket: "burj-al-ar.appspot.com",
+    messagingSenderId: "642633329988",
+    appId: "1:642633329988:web:9d0b53e56396332b003b11"
+  };
+
+  export default firebaseConfig;
